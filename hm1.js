58c130c6bfef3a5e6b28b875b232b1ba8ba9375f
Volodya <volodya.atoyan@list.ru>
2024-03-23 16:04:14 +0300

    add hm1

---
+++ hm1.js
@@ -0,0 +1,5 @@
+// Задание 1
+var celsiusTemperature = 20; // температура в градусах Цельсия
+var fahrenheitTemperature = (9 / 5) * celsiusTemperature + 32; // перевод в градусы Фаренгейта
+console.log("Температура в Цельсиях: " + celsiusTemperature + "°C");
+console.log("Температура в Фаренгейтах: " + fahrenheitTemperature + "°F");
